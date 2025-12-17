3718cd4ca589187b564c93d3f902755ae3e3e4e7
Rafael Arenas López <ra.arenas.lopez@gmail.com>
2025-12-17 15:21:16 -0300

    feat(companies): add CompanyDashboardView to module exports

--- src/frontend/views/companies/__init__.py
+++ src/frontend/views/companies/__init__.py
@@ -6,5 +6,6 @@ Vistas para gestión de empresas (listado, formularios, detalle).
 from src.frontend.views.companies.company_list_view import CompanyListView
 from src.frontend.views.companies.company_form_view import CompanyFormView
 from src.frontend.views.companies.company_detail_view import CompanyDetailView
+from src.frontend.views.companies.company_dashboard_view import CompanyDashboardView
 
-__all__ = ["CompanyListView", "CompanyFormView", "CompanyDetailView"]
+__all__ = ["CompanyListView", "CompanyFormView", "CompanyDetailView", "CompanyDashboardView"]
